a9052e1bcc521d69fc430006208dc4129a55a483
Manav Lakhani <manav@uw.edu>
2026-05-13 20:17:13 -0700

    Load data

--- D3HW1/main.js
+++ D3HW1/main.js
@@ -1,1 +1,11 @@
-//TODO
+//Load data
+d3.csv("UOF_race.csv").then(data => {
+    console.log("data", data);
+
+    //format data
+    data.forEach(d => {
+        d.Race = d.Race;
+        d["Total Incidents"] = +d["Total Incidents"];
+    });
+
+});
